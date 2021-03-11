98203f8f7a7ff92d25d94dcf0fddc9f687beac77
Benjamin Marstaller <benjamin@marstaller.org>
2021-03-11 18:03:56 +0100

    change name of slash-command to /dramabot

--- src/main/java/dramabot/slack/SlackApp.java
+++ src/main/java/dramabot/slack/SlackApp.java
@@ -74,7 +74,7 @@ public class SlackApp {
 				singleTeamBotToken(botToken).build();
 		App app = new App(appConfig);
 		app.event(AppMentionEvent.class, mentionEventHandler(catalogManager));
-		app.command("/dimmi", dimmiCommandHandler(catalogManager));
+		app.command("/dramabot", dramabotCommandHandler(catalogManager));
 		app.event(AppHomeOpenedEvent.class, getHome());
 		return app;
 	}
@@ -87,23 +87,10 @@ public class SlackApp {
 			List<CatalogEntryBean> feedbackBeans = new ArrayList<>();
 			List<CatalogEntryBean> everythingElseBeans = new ArrayList<>();
 			try {
-				List<CatalogEntryBean> allBeans = catalogManager.getBeansFromDatabase();
-				for (CatalogEntryBean catalogEntryBean : allBeans) {
-					if (catalogEntryBean.getType() != null && catalogEntryBean.getType().trim().equals("e se")) {
-						eseBeans.add(catalogEntryBean);
-					} else if (catalogEntryBean.getType() != null
-							&& catalogEntryBean.getType().trim().equals("critica")) {
-						criticaBeans.add(catalogEntryBean);
-					} else if (catalogEntryBean.getType() != null
-							&& catalogEntryBean.getType().trim().equals("feedback")) {
-						feedbackBeans.add(catalogEntryBean);
-					} else {
-						everythingElseBeans.add(catalogEntryBean);
-					}
-				}
+				fillBeansWithDatabaseContent(catalogManager, eseBeans, criticaBeans, feedbackBeans,
+						everythingElseBeans);
 			} catch (Exception e) {
 				logger.error(e.getMessage());
-				;
 			}
 			String payloadText = event.getText();
 			String username = event.getUsername();
@@ -128,6 +115,68 @@ public class SlackApp {
 		};
 	}
 
+	private static SlashCommandHandler dramabotCommandHandler(CatalogManager catalogManager) {
+
+		SlashCommandHandler slashCommandHandler = (req, ctx) -> {
+			List<CatalogEntryBean> eseBeans = new ArrayList<>();
+			List<CatalogEntryBean> criticaBeans = new ArrayList<>();
+			List<CatalogEntryBean> feedbackBeans = new ArrayList<>();
+			List<CatalogEntryBean> everythingElseBeans = new ArrayList<>();
+			fillBeansWithDatabaseContent(catalogManager, eseBeans, criticaBeans, feedbackBeans, everythingElseBeans);
+			SlashCommandPayload payload = req.getPayload();
+			String userId = payload.getUserId();
+			String userName = payload.getUserName();
+			String channelId = payload.getChannelId();
+			String channelName = payload.getChannelName();
+			String command = payload.getCommand();
+			String payloadText = payload.getText();
+			String responseUrl = payload.getResponseUrl();
+
+			StringBuilder resultBuilder = new StringBuilder();
+			
+			// default response in channel
+			String responseType = "in_channel";
+
+			logger.info(
+					"In channel {} a command '{}' " + "was sent by {}. The text was '{}', as "
+							+ "response url '{}' was given. UserId: {} ChannelId:{}",
+					channelName, command, userName, payloadText, responseUrl, userId, channelId);
+			responseType = appendPayload(eseBeans, criticaBeans, feedbackBeans, everythingElseBeans, payloadText,
+					resultBuilder, responseType);
+			String text = resultBuilder.toString();
+			SlashCommandResponse response = SlashCommandResponse.builder().text(text).responseType(responseType)
+					.build();
+			return ctx.ack(response);
+		};
+		return slashCommandHandler;
+	}
+	
+
+	private static BoltEventHandler<AppHomeOpenedEvent> getHome() {
+		return (payload, ctx) -> {
+			// Build a Home tab view
+			ZonedDateTime now = ZonedDateTime.now();
+			View appHomeView = view(view -> view.type("home").blocks(asBlocks(
+					section(section -> section.text(markdownText(
+							mt -> mt.text(":wave: Ciao, benvenut* a casa del* dramabot! (Last updated: " + now + ")")))),
+					image(img -> img
+							.imageUrl("https://www.matearium.it/wp-content/uploads/2020/02/tondo_bianco.png")))));
+			// Update the App Home for the given user
+			ViewsPublishResponse res = ctx.client().viewsPublish(
+					r -> r.userId(payload.getEvent().getUser()).hash(payload.getEvent().getView().getHash()) // To
+																												// protect
+																												// against
+																												// possible
+																												// race
+																												// conditions
+							.view(appHomeView));
+			if (!res.isOk()) {
+				logger.error("Home response was not ok");
+			}
+			return ctx.ack();
+		};
+	}
+	
 	private static String appendPayload(List<CatalogEntryBean> eseBeans, List<CatalogEntryBean> criticaBeans,
 			List<CatalogEntryBean> feedbackBeans, List<CatalogEntryBean> everythingElseBeans, String payloadText,
 			StringBuilder resultBuilder, String responseType) {
@@ -189,76 +238,23 @@ public class SlackApp {
 		return responseType;
 	}
 
-	private static BoltEventHandler<AppHomeOpenedEvent> getHome() {
-		return (payload, ctx) -> {
-			// Build a Home tab view
-			ZonedDateTime now = ZonedDateTime.now();
-			View appHomeView = view(view -> view.type("home").blocks(asBlocks(
-					section(section -> section.text(markdownText(
-							mt -> mt.text(":wave: Ciao, Bienvenuto a casa di Dramabot! (Last updated: " + now + ")")))),
-					image(img -> img
-							.imageUrl("https://www.matearium.it/wp-content/uploads/2020/02/tondo_bianco.png")))));
-			// Update the App Home for the given user
-			ViewsPublishResponse res = ctx.client().viewsPublish(
-					r -> r.userId(payload.getEvent().getUser()).hash(payload.getEvent().getView().getHash()) // To
-																												// protect
-																												// against
-																												// possible
-																												// race
-																												// conditions
-							.view(appHomeView));
-			if (!res.isOk()) {
-				logger.error("Home response was not ok");
+
+
+	private static void fillBeansWithDatabaseContent(CatalogManager catalogManager, List<CatalogEntryBean> eseBeans,
+			List<CatalogEntryBean> criticaBeans, List<CatalogEntryBean> feedbackBeans,
+			List<CatalogEntryBean> everythingElseBeans) {
+		List<CatalogEntryBean> allBeans = catalogManager.getBeansFromDatabase();
+		for (CatalogEntryBean catalogEntryBean : allBeans) {
+			if (catalogEntryBean.getType() != null && catalogEntryBean.getType().trim().equals("e se")) {
+				eseBeans.add(catalogEntryBean);
+			} else if (catalogEntryBean.getType() != null && catalogEntryBean.getType().trim().equals("critica")) {
+				criticaBeans.add(catalogEntryBean);
+			} else if (catalogEntryBean.getType() != null && catalogEntryBean.getType().trim().equals("feedback")) {
+				feedbackBeans.add(catalogEntryBean);
+			} else {
+				everythingElseBeans.add(catalogEntryBean);
 			}
-			return ctx.ack();
-		};
-	}
-
-	private static SlashCommandHandler dimmiCommandHandler(CatalogManager catalogManager) {
-
-		SlashCommandHandler slashCommandHandler = (req, ctx) -> {
-			List<CatalogEntryBean> eseBeans = new ArrayList<>();
-			List<CatalogEntryBean> criticaBeans = new ArrayList<>();
-			List<CatalogEntryBean> feedbackBeans = new ArrayList<>();
-			List<CatalogEntryBean> everythingElseBeans = new ArrayList<>();
-			List<CatalogEntryBean> allBeans = catalogManager.getBeansFromDatabase();
-			for (CatalogEntryBean catalogEntryBean : allBeans) {
-				if (catalogEntryBean.getType() != null && catalogEntryBean.getType().trim().equals("e se")) {
-					eseBeans.add(catalogEntryBean);
-				} else if (catalogEntryBean.getType() != null && catalogEntryBean.getType().trim().equals("critica")) {
-					criticaBeans.add(catalogEntryBean);
-				} else if (catalogEntryBean.getType() != null && catalogEntryBean.getType().trim().equals("feedback")) {
-					feedbackBeans.add(catalogEntryBean);
-				} else {
-					everythingElseBeans.add(catalogEntryBean);
-				}
-			}
-			SlashCommandPayload payload = req.getPayload();
-			String userId = payload.getUserId();
-			String userName = payload.getUserName();
-			String channelId = payload.getChannelId();
-			String channelName = payload.getChannelName();
-			String command = payload.getCommand();
-			String payloadText = payload.getText();
-			String responseUrl = payload.getResponseUrl();
-
-			StringBuilder resultBuilder = new StringBuilder();
-			
-			// default response in channel
-			String responseType = "in_channel";
-
-			logger.info(
-					"In channel {} a command '{}' " + "was sent by {}. The text was '{}', as "
-							+ "response url '{}' was given. UserId: {} ChannelId:{}",
-					channelName, command, userName, payloadText, responseUrl, userId, channelId);
-			responseType = appendPayload(eseBeans, criticaBeans, feedbackBeans, everythingElseBeans, payloadText,
-					resultBuilder, responseType);
-			String text = resultBuilder.toString();
-			SlashCommandResponse response = SlashCommandResponse.builder().text(text).responseType(responseType)
-					.build();
-			return ctx.ack(response);
-		};
-		return slashCommandHandler;
+		}
 	}
 
 }
