f897a57ee804928a38a19749b1384b3bb0d6b7e8
Benjamin Marstaller <benjamin@marstaller.org>
2021-04-05 19:05:07 +0200

    Get /dramabot to work

--- src/main/java/dramabot/service/SlackCommandManager.java
+++ src/main/java/dramabot/service/SlackCommandManager.java
@@ -2,6 +2,7 @@ package dramabot.service;
 
 import com.slack.api.app_backend.slash_commands.payload.SlashCommandPayload;
 import com.slack.api.bolt.handler.builtin.SlashCommandHandler;
+import com.slack.api.methods.AsyncMethodsClient;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import dramabot.service.model.CatalogEntryBean;
@@ -35,12 +36,13 @@ public class SlackCommandManager {
 
     public SlashCommandHandler dramabotCommandHandler() {
         return (req, ctx) -> {
-            executorService.execute(() -> createAsyncDramabotResponse(req, ctx));
+            AsyncMethodsClient client = ctx.asyncClient();
+            executorService.execute(() -> createAsyncDramabotResponse(req, client));
             return ctx.ack();
         };
     }
 
-    private void createAsyncDramabotResponse(com.slack.api.bolt.request.builtin.SlashCommandRequest req, com.slack.api.bolt.context.builtin.SlashCommandContext ctx) {
+    private void createAsyncDramabotResponse(com.slack.api.bolt.request.builtin.SlashCommandRequest req, AsyncMethodsClient client) {
         Map<String, List<CatalogEntryBean>> authors = new HashMap<>();
         Map<String, List<CatalogEntryBean>> allBeans = SlackManagerUtils.fillAuthorsAndReturnAllBeansWithDatabaseContent(authors, catalogManager);
         SlashCommandPayload payload = req.getPayload();
@@ -50,27 +52,29 @@ public class SlackCommandManager {
         String channelName = payload.getChannelName();
         String command = payload.getCommand();
         String payloadText = payload.getText();
+/*
         String responseUrl = payload.getResponseUrl();
+*/
 
         StringBuilder resultBuilder = new StringBuilder();
 
         // default response in channel
         String responseType = SlackApp.IN_CHANNEL;
 
-        logger.debug("In channel {} a command '{}' " + "was sent by {}. The text was '{}', as "
-                        + "response url '{}' was given. UserId: {} ChannelId:{}",
-                channelName, command, userName, payloadText, responseUrl, userId, channelId);
+        logger.debug("In channel {} '{}' " + "was sent by {}. The text was '{}', with UserId: {} ChannelId:{}",
+                channelName, command, userName, payloadText, userId, channelId);
         if (!payloadText.contains("catalogo")) {
             SlackManagerUtils.appendPayload(authors, allBeans, payloadText,
                     resultBuilder, responseType);
             String text = resultBuilder.toString();
+            logger.debug("answer is: {}", text);
             String iconEmoji = payloadText.contains(" amo") ? ":heart:" : null;
-            ChatPostMessageRequest asyncRequest = ChatPostMessageRequest.builder().text(text).channel(ctx.getChannelId())
+            ChatPostMessageRequest asyncRequest = ChatPostMessageRequest.builder().text(text).channel(channelId)
                     .iconEmoji(iconEmoji).token(botToken).build();
-            ctx.asyncClient().chatPostMessage(asyncRequest);
+            client.chatPostMessage(asyncRequest);
         } else {
             try {
-                SlackManagerUtils.doCatalogCsvResponse(ctx.asyncClient(), userId, channelId, botToken);
+                SlackManagerUtils.doCatalogCsvResponse(client, userId, channelId, botToken);
             } catch (IOException | SlackApiException e) {
                 logger.debug("Error in /dramabot - command while searching for catalog", e);
             }
